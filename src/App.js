b2890d41a284e31ee53f5adfdaa0d525fb01e7bc
Nayan <nayanrahul.jnv@gmail.com>
2022-06-01 15:00:18 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {Navbar} from './Navbar';
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+            
+            <div className="test">
+                <label htmlFor="test">Hi There its a label</label>
+            </div>
+        </div>
+    );
+}
+
+export default App;
