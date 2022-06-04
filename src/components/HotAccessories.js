1cc79e0640c13763d45cb9d16b110127eab116fe
Nayan <nayanrahul.jnv@gmail.com>
2022-06-04 12:25:01 +0530

    Add blank card

--- src/components/HotAccessories.js
+++ src/components/HotAccessories.js
@@ -14,6 +14,7 @@ const HotAccessories = (data) => {
                         return <HotItemCard key={index} image={item.image} imageName={item.name} price={item.price}></HotItemCard>
                     })
                 }
+                <HotItemCard image={'https://i01.appmifile.com/webfile/globalimg/in/cms/593181BE-7DB8-15F5-E907-506427B8C9F4.jpg'}></HotItemCard>
             </div>
         </div>
     )
